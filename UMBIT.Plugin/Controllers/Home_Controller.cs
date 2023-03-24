d9f7d5e8a9d974936669ac7575e57ad1a626eb3a
Leonardo Machado da Silva <machadoleonardo7856@gmail.com>
2023-03-23 23:12:56 -0300

    Fist Commit

---
+++ UMBIT.Plugin/Controllers/Home_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using UMBIT.Plugin.Models;
+
+namespace UMBIT.Plugin.Controllers
+{
+    public class Home_Controller : Controller
+    {
+        private readonly ILogger<Home_Controller> _logger;
+
+        public Home_Controller(ILogger<Home_Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
