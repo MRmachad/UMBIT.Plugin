3c2c08df5b091d7ba9e1433202f36a544f667c77
Leonardo Machado da Silva <machadoleonardo7856@gmail.com>
2023-03-31 00:11:44 -0300

    beta versão

---
+++ UMBIT.Plugin/Registration/Init.cs
@@ -0,0 +1,11 @@
+﻿using UMBIT.MVC.Core.Configurate.Initializable;
+
+namespace UMBIT.Plugin.Registration
+{
+    public class Init : IModuleInitializer
+    {
+        void IModuleInitializer.Init(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
